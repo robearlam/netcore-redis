6acce673dd231b5ac4bde701120d00421771e84b
Rob Earlam <robert.earlam@gmail.com>
2017-08-15 16:10:45 +1000

    Console app running

--- NetCoreRedis.ConsoleApp/Program.cs
+++ NetCoreRedis.ConsoleApp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Redis;
+using Microsoft.Extensions.Options;
 
 namespace NetCoreRedis.ConsoleApp
 {
@@ -6,7 +9,23 @@ namespace NetCoreRedis.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IDistributedCache cache = new RedisCache(new OptionsWrapper<RedisCacheOptions>(new RedisCacheOptions
+            {
+                Configuration = "127.0.0.1:32769",
+                InstanceName = "master"
+            }));
+
+            var prefix = "cacheKeyPrefix_";
+            for (var i = 0; i <= 99; i++)
+            {
+                cache.SetString($"{prefix}{i}", DateTime.Now.Ticks.ToString());
+                Console.WriteLine($"Writing to cache: {i}");
+            }
+
+            for (var i = 0; i <= 99; i++)
+            {
+                Console.WriteLine(cache.GetString($"{prefix}{i}"));
+            }
         }
     }
 }
